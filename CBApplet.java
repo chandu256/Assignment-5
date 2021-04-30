bd0bbea3d7db269b425ce47616c6c35d82bc7a81
chandu256 <81565889+chandu256@users.noreply.github.com>
2021-04-30 20:21:31 +0530

    Add files via upload

---
+++ CBApplet.java
@@ -0,0 +1,29 @@
+/*<applet code="CBApplet.class" width=300 height=300>
+</applet>*/
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.*;
+public class CBApplet extends Applet implements ItemListener
+{
+Choice c;
+public void init()
+{
+Label l=new Label("select any oneItem");
+c=new Choice();
+c.add("red");c.add("green");c.add("blue");
+c.addItemListener(this);
+add(l);add(c);
+}
+public void itemStateChanged(ItemEvent ae)
+{
+String s=c.getSelectedItem();
+if(s.equals("red"))
+setBackground(Color.red);
+else if(s.equals("green"))
+setBackground(Color.green);
+else
+setBackground(Color.blue);
+repaint();
+}
+}
+
